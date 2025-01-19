7b48f0cb61cf8e61f573789e91eb6a824913815a
asuka.home <asuka.home@xxx.cn>
2025-01-19 10:34:50 +0800

    point start

--- chapter06_point/pointTest.c
+++ chapter06_point/pointTest.c
@@ -4,7 +4,7 @@ void main()
 {
     int *p1; // 定义指针
     int a = 10;
-    p1 = &a;          // 取地址运算符
+    p1 = &a;            // 取地址运算符
     printf("%p\n", p1); // 0061FF18 内存地址
 
     printf("%d\n", *p1); // 取值运算符
